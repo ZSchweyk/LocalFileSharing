966f30e029df7a4f5b45db908d0ec6e77a5941cb
ZSchweyk <zeynschweyk@dpengineering.org>
2022-05-31 12:11:37 -0700

    it's working for files that aren't named exactly the same. Otherwise, i have no idea what's going on. It's somehow remembering files that I've already deleted

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
+import datetime
+
 from flask import Flask, send_file, render_template, request, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import FileField
 from flask_uploads import configure_uploads, IMAGES, UploadSet
+from os import listdir
+from os.path import isfile, isdir, join
+
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "thisisasecret"
-app.config["UPLOADED_IMAGES_DEST"] = "/home/pi/projects/LocalFileSharing/Uploads"
+app.config["UPLOADED_IMAGES_DEST"] = "/home/pi/projects/LocalFileSharing/Uploads/"
 
 
 images = UploadSet("images", IMAGES)
@@ -17,17 +22,37 @@ class MyForm(FlaskForm):
     image = FileField("image")
 
 
+
+
 @app.route("/", methods=["GET", "POST"])
-def index():
+def upload():
     form = MyForm()
 
     if form.validate_on_submit():
         print(form.image.data)
         filename = images.save(form.image.data)
-        return "Filename: " + filename
+        return redirect(url_for("files"))
 
-    return render_template("index.html", form=form)
+    return render_template("upload.html", form=form)
 
+
+@app.route("/files", methods=["GET", "POST"])
+def files():
+    all_files = []
+    for child in listdir(app.config["UPLOADED_IMAGES_DEST"]):
+        if isfile(join(app.config["UPLOADED_IMAGES_DEST"], child)):
+            all_files.append(child)
+
+    print("rendering files.html template")
+    print(all_files)
+    print(datetime.datetime.now().strftime("%I:%M:%S %p"))
+    return render_template("files.html", all_files=all_files)
+
+
+@app.route("/download")
+def download():
+    file = request.args["file"]
+    return send_file(join(app.config["UPLOADED_IMAGES_DEST"], file), as_attachment=True)
 
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,4 +0,0 @@
-all_files = []
-    for child in listdir(app.config["UPLOADED_IMAGES_DEST"]):
-        if isfile(child):
-            all_files.append(child)
